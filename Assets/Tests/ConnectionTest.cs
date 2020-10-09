4c566d0b9db44f81b054fe8d4e573ac33b35835e
Paul Pacheco <paul.pacheco@aa.com>
2020-10-08 23:11:00 -0500

    Test that we can send some data

--- Assets/Tests/ConnectionTest.cs
+++ Assets/Tests/ConnectionTest.cs
@@ -2,6 +2,11 @@
 using NUnit.Framework;
 using UnityEngine.TestTools;
 using Cysharp.Threading.Tasks;
+using System;
+using System.IO;
+
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Mirror.KCP
 {
@@ -25,7 +30,29 @@ namespace Mirror.KCP
             IConnection connection = await server.AcceptAsync();
 
             Assert.That(connection, Is.Not.Null);
+
+            server.Shutdown();
         });
 
+
+        [UnityTest]
+        public IEnumerator CanSendData() => UniTask.ToCoroutine(async () =>
+        {
+            server = new Server();
+            await server.Start(PORT);
+
+            client = new KcpConnection(null);
+
+            await client.Connect("localhost", PORT);
+            IConnection connection = await server.AcceptAsync();
+            byte[] data = new byte[] { (byte)Random.Range(1,255) };
+            await client.SendAsync(new ArraySegment<byte>(data));
+
+            var buffer = new MemoryStream();
+            await connection.ReceiveAsync(buffer);
+            Assert.That(buffer.ToArray(), Is.EquivalentTo(data));
+
+            server.Shutdown();
+        });
     }
 }
